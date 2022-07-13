8bbeade18ef72ee4c8df8d3b44fbab660accadef
daniel.szopa333 <daniel.szopa333@gmail.com>
2022-07-13 08:34:24 +0200

    Disable nullable references

--- Comics.ApplicationCore/Features/Registration/RegisterAccount.cs
+++ Comics.ApplicationCore/Features/Registration/RegisterAccount.cs
@@ -77,12 +77,12 @@ namespace Comics.ApplicationCore.Features.Registration
 
     public class RegisterAccountRequest : IRequest
     {
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
-        public string? UserName { get; set; }
-        public string? Email { get; set; }
-        public string? Password { get; set; }
-        public string? ConfirmPassword { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
     }
 
     public class RegisterAccountRequestHandler : IRequestHandler<RegisterAccountRequest>
